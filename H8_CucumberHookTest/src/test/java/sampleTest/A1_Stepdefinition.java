ba7e94b52ee798aef30457994e55e02bdf30e171
MarirsRam <sriramsara95@gmail.com>
2022-09-13 20:20:45 +0530

    my second commit

---
+++ H8_CucumberHookTest/src/test/java/sampleTest/A1_Stepdefinition.java
@@ -0,0 +1,16 @@
+package sampleTest;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.But;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class A1_Stepdefinition {
+
+//	@Before(order=1)
+//	public void 
+	
+//}
+}
+
---
+++ H8_CucumberHookTest/src/test/java/sampleTest/A2_RunnerTest.java
@@ -0,0 +1,16 @@
+package sampleTest;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+// dependency - junit + cucumber.junit
+
+@RunWith(Cucumber.class)
+@CucumberOptions(tags="@login")
+
+public class A2_RunnerTest {
+	
+
+}
